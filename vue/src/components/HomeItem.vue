444ab87263a6cd6bf502246ce406678b4a0a0ddf
Zkejm <miroslav.kolesar@gymmoldava.sk>
2025-04-01 20:31:21 +0200

    frontpage preview

---
+++ vue/src/components/HomeItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="item">
+    <h3>
+      <slot name="heading"></slot>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+}
+
+h3 {
+  width: 100%;
+  font-size: xx-large;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+}
+
+@media (min-width: 315px) {
+  .item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+  }
+
+  .item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+  }
+
+  .item:first-of-type:before {
+    display: none;
+  }
+
+  .item:last-of-type:after {
+    display: none;
+  }
+}
+</style>
